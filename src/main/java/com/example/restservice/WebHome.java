7cec18ea8e77a6e9abf65c60f02e485febc0b710
carghai <carghai74@gmail.com>
2024-06-02 18:23:15 -0400

    I HATE JAVA

---
+++ src/main/java/com/example/restservice/WebHome.java
@@ -0,0 +1,17 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebHome {
+	@GetMapping("/myapp")
+	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return  "home";
+	}
+}
