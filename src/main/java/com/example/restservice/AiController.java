7880cda7589eea5737eeb14ab607d3801423bdc8
carghai <carghai74@gmail.com>
2024-06-10 09:47:36 -0400

    cool

--- src/main/java/com/example/restservice/AiController.java
+++ src/main/java/com/example/restservice/AiController.java
@@ -1,6 +1,7 @@
 package com.example.restservice;
 
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,10 +25,21 @@ import org.json.JSONArray;
 @RestController
 public class AiController {
     private static HashMap<String, JSONObject> jsonRequester = new HashMap<>();
-
+    private static final AtomicInteger amount = new AtomicInteger(0);
+    private static HashMap<String, Boolean> idRateLimit = new HashMap<>();
     @GetMapping("/bot")
     public AiReponse api(@RequestParam(value = "text", defaultValue = "none") String text,
             @RequestParam(value = "id", defaultValue = "none") String id) {
+        amount.incrementAndGet();
+        if (idRateLimit.get(id) == null || !idRateLimit.get(id)) {
+            idRateLimit.put(id, true);
+        }else {
+            return new AiReponse("ID Rate Limited Reached, Please Wait");
+        }
+
+        if (amount.get() > 10) {
+            return new AiReponse("Rate Limted Reached, Please Wait");
+        }
         if (text.isBlank()) {
             return new AiReponse("");
         }
@@ -69,8 +81,12 @@ public class AiController {
             newMessage.put("role", "assistant");
             newMessage.put("content", content);
             jsonRequester.get(id).getJSONArray("messages").put(newMessage);
+            amount.decrementAndGet();
+            idRateLimit.put(id, false);
             return new AiReponse(content);
         } catch (Exception e) {
+            amount.decrementAndGet();
+            idRateLimit.put(id, false);
             return new AiReponse(e.toString());
         }
     }
